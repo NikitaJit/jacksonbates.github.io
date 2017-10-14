25a393fc19b65c89b5d4b9df36d3bdc4d2f3a487
jacksonbates <malgalin@gmail.com>
2017-10-14 21:48:18 +1100

    hello worlds react

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Portfolio from './components/Portfolio';
+// import 'normalize.css/normalize.css';
+// import './styles/styles.scss';
+
+ReactDOM.render(<Portfolio />, document.getElementById('app'));
