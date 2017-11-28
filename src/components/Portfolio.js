ea17e785b2dbed29532504ffd2b9853592981ded
jacksonbates <malgalin@gmail.com>
2017-11-29 00:06:22 +1100

    wip - fetch working

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 export default class Portfolio extends React.Component {
+  state = {
+    portfolio: []
+  }
+  componentDidMount() {
+    const url = 'https://api.github.com/users/jacksonbates/repos';
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState(() => ({
+          portfolio: data
+        }));
+      });
+  }
   render() {
     return (
       <div>
-        <h1>Hello, world</h1>
+        <ul>
+          {this.state.portfolio.map((x) => {
+          return (
+            <li>{x.name}</li>
+          )},)}
+        </ul>
       </div>
-    )
-  }
-}
+        )}}
+    
